aa7c06c24d1ee91e97652944df006a1cbfcc4e78
Walyson Scarazzati da Silva <53382989+walyson-scarazzati@users.noreply.github.com>
2025-11-19 20:45:52 +0000

    15 - Tratamento de Exceções no Kafka

---
+++ str-consumer/src/main/java/com/valdir/strconsumer/exceptions/ErrorCustomHandler.java
@@ -0,0 +1,17 @@
+package com.valdir.strconsumer.exceptions;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+import org.springframework.kafka.listener.ListenerExecutionFailedException;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class ErrorCustomHandler implements KafkaListenerErrorHandler {
+    @Override
+    public Object handleError(Message<?> message, ListenerExecutionFailedException exception) {
+        log.info("EXCEPTION HANDLER::: Capturei um erro");
+        return null;
+    }
+}
--- str-consumer/src/main/java/com/valdir/strconsumer/listers/StrConsumerListener.java
+++ str-consumer/src/main/java/com/valdir/strconsumer/listers/StrConsumerListener.java
@@ -1,6 +1,7 @@
 package com.valdir.strconsumer.listers;
 
 import com.valdir.strconsumer.custom.StrConsumerCustomListener;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -9,9 +10,11 @@ import org.springframework.stereotype.Component;
 @Log4j2
 @Component
 public class StrConsumerListener {
+    @SneakyThrows
     @StrConsumerCustomListener(groupId = "group-1")
     public  void create(String message) {
         log.info("Create::: Received message: {}", message);
+        throw new IllegalArgumentException("Exception ...");
     }
 
     @StrConsumerCustomListener(groupId = "group-1")
