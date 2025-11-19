faba8b1fdc67d58d0d155c94351f1e0326886e75
Walyson Scarazzati da Silva <53382989+walyson-scarazzati@users.noreply.github.com>
2025-11-19 20:39:16 +0000

    12 - Determinado particao para um consumer especifico

--- str-consumer/src/main/java/com/valdir/strconsumer/listers/StrConsumerListener.java
+++ str-consumer/src/main/java/com/valdir/strconsumer/listers/StrConsumerListener.java
@@ -2,17 +2,22 @@ package com.valdir.strconsumer.listers;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
-    @KafkaListener(topics = "str-topic", groupId = "group-0", containerFactory = "strContainerFactory")
+    @KafkaListener(topics = "str-topic", topicPartitions = {
+            @TopicPartition(topic = "str-topic", partitions = {"0"})
+    },containerFactory = "strContainerFactory")
     public  void create(String message) {
         log.info("Create::: Received message: {}", message);
     }
 
-    @KafkaListener(topics = "str-topic", groupId = "group-1", containerFactory = "strContainerFactory")
+    @KafkaListener(topics = "str-topic",  topicPartitions = {
+            @TopicPartition(topic = "str-topic", partitions = {"1"})
+    }, containerFactory = "strContainerFactory")
     public  void log(String message) {
         log.info("LOG::: Received message: {}", message);
     }
